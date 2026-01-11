72e3d5463ded52c9b1f16df2599bfb7ecdea7e4b
maycolyate <maycolyae15@gmail.com>
2026-01-11 18:13:58 -0500

    feat: complete investment calculator application

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
+const INITIAL_VALUES = {
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+}
+
 function App() {
+  const [userInput, setUserInput] = useState(INITIAL_VALUES);
+
+  function handleChange(input, value) {
+    setUserInput(prevUserInputs => {
+        return {
+            ...prevUserInputs,
+            [input]: + value
+        }
+    })
+  }
+
+  const inputIsValid = userInput.duration >= 1 ; 
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header/>
+      <UserInput userInput={userInput} onChange={handleChange}/>
+      {!inputIsValid && <p className="center">Enter a valid duration</p>}
+      {inputIsValid && <Results input={userInput}/>}
+    </>
   )
 }
 
